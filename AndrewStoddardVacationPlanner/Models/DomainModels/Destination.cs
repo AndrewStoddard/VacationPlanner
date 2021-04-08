f46316e3a35329b1cf2e131487ebf415066b14c9
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 13:47:42 -0400

    Added Destination model class

---
+++ AndrewStoddardVacationPlanner/Models/DomainModels/Destination.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndrewStoddardVacationPlanner.Models.DomainModels
+{
+    public class Destination
+    {
+        #region Properties
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Destination name is Required")]
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
