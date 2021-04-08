37f78895c34a6eb388ee175acf01e12fe628a5ce
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 14:32:05 -0400

    Added Accommodation class

---
+++ AndrewStoddardVacationPlanner/Models/DomainModels/Accommodation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndrewStoddardVacationPlanner.Models.DomainModels
+{
+    public class Accommodation
+    {
+        #region Properties
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Accommodation name is required")]
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        #endregion
+    }
+}
