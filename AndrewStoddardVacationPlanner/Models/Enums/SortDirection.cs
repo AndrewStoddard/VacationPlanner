6c5301182692dc13d7d955b72f1ed0a3d59ad55a
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 17:52:14 -0400

    Added enums for sorting

---
+++ AndrewStoddardVacationPlanner/Models/Enums/SortDirection.cs
@@ -0,0 +1,18 @@
+﻿namespace AndrewStoddardVacationPlanner.Models.Enums
+{
+    /// <summary>
+    ///     Enum SortDirection
+    /// </summary>
+    public enum SortDirection
+    {
+        /// <summary>
+        ///     The ascending
+        /// </summary>
+        Ascending = 1,
+
+        /// <summary>
+        ///     The descending
+        /// </summary>
+        Descending = 2
+    }
+}
---
+++ AndrewStoddardVacationPlanner/Models/Enums/SortType.cs
@@ -0,0 +1,11 @@
+﻿namespace AndrewStoddardVacationPlanner.Models.Enums
+{
+    public enum SortType
+    {
+        SortByDestination = 1,
+        SortByStartDate = 2,
+        SortByEndDate = 3,
+        SortByAccommodation = 4,
+        SortByActivities = 5
+    }
+}
