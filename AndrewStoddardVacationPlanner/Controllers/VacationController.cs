bab99457258efb2b168a1173c186159ffb8d8504
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 16:01:37 -0400

    Added Controller and its dependency injections in startup

---
+++ AndrewStoddardVacationPlanner/Controllers/VacationController.cs
@@ -0,0 +1,23 @@
+﻿using AndrewStoddardVacationPlanner.Models.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndrewStoddardVacationPlanner.Controllers
+{
+    public class VacationController : Controller
+    {
+        #region Data members
+
+        private IUnitOfWork unitOfWork;
+
+        #endregion
+
+        #region Constructors
+
+        public VacationController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        #endregion
+    }
+}
--- AndrewStoddardVacationPlanner/Startup.cs
+++ AndrewStoddardVacationPlanner/Startup.cs
@@ -1,5 +1,8 @@
+using AndrewStoddardVacationPlanner.Models.DataAccessLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -7,6 +10,21 @@ namespace AndrewStoddardVacationPlanner
 {
     public class Startup
     {
+        #region Properties
+
+        public IConfiguration Configuration { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        #endregion
+
         #region Methods
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -14,6 +32,9 @@ namespace AndrewStoddardVacationPlanner
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<VacationContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("SalesContext")));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,13 +45,15 @@ namespace AndrewStoddardVacationPlanner
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     "default",
-                    "{controller=Home}/{action=Index}/");
+                    "{controller=Vacation}/{action=Index}/");
             });
         }
 
