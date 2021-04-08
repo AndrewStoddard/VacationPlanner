47f5fbf8d43b4e83dfa74e31608febd4c4ac2b28
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 14:49:48 -0400

    Added the VacationContext without seed data/configs

---
+++ AndrewStoddardVacationPlanner/Models/DataAccessLayer/VacationContext.cs
@@ -0,0 +1,33 @@
+﻿using AndrewStoddardVacationPlanner.Models.DomainModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace AndrewStoddardVacationPlanner.Models.DataAccessLayer
+{
+    public class VacationContext : DbContext
+    {
+        #region Properties
+
+        public DbSet<Accommodation> Accommodations { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public VacationContext(DbContextOptions<VacationContext> options) : base(options)
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        #endregion
+    }
+}
