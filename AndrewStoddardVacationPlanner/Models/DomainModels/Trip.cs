4cc0a33ee424fcc5bc228076e37a77c244097264
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 14:44:00 -0400

    Added a Trip model class

---
+++ AndrewStoddardVacationPlanner/Models/DomainModels/Trip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AndrewStoddardVacationPlanner.Models.DomainModels
+{
+    public class Trip
+    {
+        #region Properties
+
+        public int Id { get; set; }
+
+        [Required] public int DestinationId { get; set; }
+
+        public Destination Destination { get; set; }
+
+        [Required] public int AccommodationId { get; set; }
+
+        public Accommodation Accommodation { get; set; }
+
+        [Required] public virtual ICollection<Activity> Activities { get; set; }
+
+        [Required] [DataType(DataType.Date)] public DateTime StartDate { get; set; }
+
+        [Required] [DataType(DataType.Date)] public DateTime EndDate { get; set; }
+
+        #endregion
+    }
+}
