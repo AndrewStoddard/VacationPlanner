62a411a11959e0355b9f5e945d63cd9740644cc7
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 15:53:08 -0400

    Added the unit of work and interface for it

---
+++ AndrewStoddardVacationPlanner/Models/DataAccessLayer/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using AndrewStoddardVacationPlanner.Models.DomainModels;
+
+namespace AndrewStoddardVacationPlanner.Models.DataAccessLayer
+{
+    public interface IUnitOfWork
+    {
+        #region Properties
+
+        IRepository<Accommodation> Accommodations { get; }
+        IRepository<Activity> Activities { get; }
+        IRepository<Destination> Destinations { get; }
+        IRepository<Trip> Trips { get; }
+
+        #endregion
+
+        #region Methods
+
+        void Save();
+
+        #endregion
+    }
+}
---
+++ AndrewStoddardVacationPlanner/Models/DataAccessLayer/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using AndrewStoddardVacationPlanner.Models.DomainModels;
+
+namespace AndrewStoddardVacationPlanner.Models.DataAccessLayer
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        #region Data members
+
+        private readonly VacationContext context;
+
+        #endregion
+
+        #region Properties
+
+        public IRepository<Accommodation> Accommodations { get; }
+        public IRepository<Activity> Activities { get; }
+        public IRepository<Destination> Destinations { get; }
+        public IRepository<Trip> Trips { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public UnitOfWork(VacationContext context)
+        {
+            this.context = context;
+            this.Accommodations = new Repository<Accommodation>(context);
+            this.Activities = new Repository<Activity>(context);
+            this.Destinations = new Repository<Destination>(context);
+            this.Trips = new Repository<Trip>(context);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        #endregion
+    }
+}
