943ae737af2c4e78e0a784865d03913a2e7e34ed
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 17:46:35 -0400

    Added View Model for Home page called Vacation List View Model

---
+++ AndrewStoddardVacationPlanner/Models/ViewModels/VacationListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using AndrewStoddardVacationPlanner.Models.DomainModels;
+
+namespace AndrewStoddardVacationPlanner.Models.ViewModels
+{
+    public class VacationListViewModel
+    {
+        #region Properties
+
+        public List<Trip> Trips { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+        public int[] PageSizes { get; } = {1, 2, 3, 4, 5};
+
+        #endregion
+    }
+}
