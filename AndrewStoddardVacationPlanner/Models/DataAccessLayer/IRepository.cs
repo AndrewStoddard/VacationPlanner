ab00e90a396db38fbd8dd91f5c93012cc18196bc
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 15:44:17 -0400

    Added the Repository and interface for it

---
+++ AndrewStoddardVacationPlanner/Models/DataAccessLayer/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AndrewStoddardVacationPlanner.Models.DataAccessLayer
+{
+    public interface IRepository<T> where T : class
+    {
+        #region Methods
+
+        IQueryable<T> Get(Expression<Func<T, bool>> expression = null);
+
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        #endregion
+    }
+}
---
+++ AndrewStoddardVacationPlanner/Models/DataAccessLayer/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AndrewStoddardVacationPlanner.Models.DataAccessLayer
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        #region Data members
+
+        protected VacationContext context;
+
+        #endregion
+
+        #region Constructors
+
+        public Repository(VacationContext context)
+        {
+            this.context = context;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public virtual IQueryable<T> Get(Expression<Func<T, bool>> expression = null)
+        {
+            return expression == null ? this.context.Set<T>() : this.context.Set<T>().Where(expression);
+        }
+
+        public virtual void Insert(T entity)
+        {
+            this.context.Set<T>().Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            this.context.Set<T>().Update(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            this.context.Set<T>().Remove(entity);
+        }
+
+        #endregion
+    }
+}
