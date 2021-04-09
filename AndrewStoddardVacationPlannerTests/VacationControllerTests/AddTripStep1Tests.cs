5bc860dfd6a6bdaba2b58f214c2ddfac8f853629
AndrewStoddard <acs8182000@gmail.com>
2021-04-09 00:40:11 -0400

    Added Add trip step 1 tests

---
+++ AndrewStoddardVacationPlannerTests/VacationControllerTests/AddTripStep1Tests.cs
@@ -0,0 +1,24 @@
+﻿using AndrewStoddardVacationPlanner.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace AndrewStoddardVacationPlannerTests.VacationControllerTests
+{
+    public class AddTripStep1Tests
+    {
+        #region Methods
+
+        [Fact]
+        public void AddTripStep1_ReturnsViewResult()
+        {
+            var accessor = TestHelperLibrary.SetUpHttpContextAccessor();
+            var unitOfWork = TestHelperLibrary.SetupUnitOfWork();
+            var controller = new VacationController(unitOfWork.Object, accessor.Object);
+
+            var result = controller.AddTripStep1() as ViewResult;
+            Assert.IsType<ViewResult>(result);
+        }
+
+        #endregion
+    }
+}
