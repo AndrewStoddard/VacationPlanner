6c8ba9b75375d7ca187956a469cb7ee07d759ee7
AndrewStoddard <acs8182000@gmail.com>
2021-04-09 00:40:38 -0400

    Added Delete trip tests

---
+++ AndrewStoddardVacationPlannerTests/VacationControllerTests/DeleteTripTests.cs
@@ -0,0 +1,28 @@
+﻿using AndrewStoddardVacationPlanner.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using Xunit;
+
+namespace AndrewStoddardVacationPlannerTests.VacationControllerTests
+{
+    public class DeleteTripTests
+    {
+        #region Methods
+
+        [Fact]
+        public void DeleteTrip_Delete1_ReturnsRedirectToActionResult()
+        {
+            var accessor = TestHelperLibrary.SetUpHttpContextAccessor();
+            var unitOfWork = TestHelperLibrary.SetupUnitOfWork();
+            var tempData = new TempDataDictionary(accessor.Object.HttpContext, Mock.Of<ITempDataProvider>());
+            var controller = new VacationController(unitOfWork.Object, accessor.Object);
+            controller.TempData = tempData;
+
+            var result = controller.DeleteTrip(1) as RedirectToActionResult;
+            Assert.IsType<RedirectToActionResult>(result);
+        }
+
+        #endregion
+    }
+}
