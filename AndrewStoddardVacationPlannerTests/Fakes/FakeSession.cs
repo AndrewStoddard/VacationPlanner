2cbf39e48892600d2fc0719bc50137df18e5025e
AndrewStoddard <acs8182000@gmail.com>
2021-04-09 00:14:20 -0400

    Added fake Session and helper methods to do the DI each test

---
+++ AndrewStoddardVacationPlannerTests/Fakes/FakeSession.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AndrewStoddardVacationPlannerTests.Fakes
+{
+    public class FakeSession : ISession
+    {
+        #region Data members
+
+        private readonly Dictionary<string, object> sessionStorage = new();
+
+        #endregion
+
+        #region Properties
+
+        public object this[string name]
+        {
+            get => this.sessionStorage[name];
+            set => this.sessionStorage[name] = value;
+        }
+
+        public bool IsAvailable { get; }
+        public string Id { get; }
+        public IEnumerable<string> Keys { get; }
+
+        #endregion
+
+        #region Methods
+
+        public Task LoadAsync(CancellationToken cancellationToken = new())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CommitAsync(CancellationToken cancellationToken = new())
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(string key, out byte[] value)
+        {
+            try
+            {
+                if (this.sessionStorage[key] != null)
+                {
+                    value = Encoding.BigEndianUnicode.GetBytes(this.sessionStorage[key].ToString());
+                    return true;
+                }
+
+                value = null;
+                return false;
+            }
+            catch (Exception e)
+            {
+                value = null;
+                return false;
+            }
+        }
+
+        public void Set(string key, byte[] value)
+        {
+            this.sessionStorage[key] = Encoding.BigEndianUnicode.GetString(value);
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ AndrewStoddardVacationPlannerTests/TestHelperLibrary.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AndrewStoddardVacationPlanner.Models.DataAccessLayer;
+using AndrewStoddardVacationPlanner.Models.DomainModels;
+using AndrewStoddardVacationPlannerTests.Fakes;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace AndrewStoddardVacationPlannerTests
+{
+    public class TestHelperLibrary
+    {
+        #region Methods
+
+        public static Mock<IUnitOfWork> SetupUnitOfWork()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            var acc1 = new Accommodation {
+                Id = 1,
+                Name = "High-Rise Condo",
+                Email = "book@highrise.com",
+                PhoneNumber = "1234567890"
+            };
+            var acc2 = new Accommodation {
+                Id = 2,
+                Name = "Low-Rise Condo",
+                Email = "book@lowrise.com",
+                PhoneNumber = "0987654321"
+            };
+            var acc3 = new Accommodation {
+                Id = 3,
+                Name = "Motel 8",
+                Email = "book@motel8.com",
+                PhoneNumber = "5632147890"
+            };
+            var acc4 = new Accommodation {
+                Id = 4,
+                Name = "Holiday Inn",
+                Email = "book@holidayinn.com",
+                PhoneNumber = "5823694170"
+            };
+            var accs = new List<Accommodation> {
+                acc1, acc2, acc3, acc4
+            };
+
+            var act1 = new Activity {Id = 1, Name = "SkyDiving"};
+            var act2 = new Activity {Id = 2, Name = "Parasail"};
+            var act3 = new Activity {Id = 3, Name = "Go Karts"};
+            var act4 = new Activity {Id = 4, Name = "Mini Golf"};
+            var act5 = new Activity {Id = 5, Name = "Mountain Hiking"};
+            var act6 = new Activity {Id = 6, Name = "Deep Sea Fishing"};
+
+            var acts = new List<Activity> {
+                act1, act2, act3, act4, act5, act6
+            };
+
+            var dest1 = new Destination {Id = 1, Name = "Orlando, Florida"};
+            var dest2 = new Destination {Id = 2, Name = "Panama City, Florida"};
+            var dest3 = new Destination {Id = 3, Name = "Orange Beach, Alabama"};
+            var dest4 = new Destination {Id = 4, Name = "Helen, Georgia"};
+            var dest5 = new Destination {Id = 5, Name = "New York City, New York"};
+            var dest6 = new Destination {Id = 6, Name = "Gatlinburg, Tennessee"};
+
+            var dests = new List<Destination> {
+                dest1, dest2, dest3, dest4, dest5, dest6
+            };
+
+            var trip1 = new Trip {
+                Id = 1,
+                DestinationId = 6,
+                AccommodationId = 4,
+                StartDate = new DateTime(2019, 12, 20),
+                EndDate = new DateTime(2020, 1, 2),
+                Activities = new List<Activity> {act2, act4}
+            };
+            var trip2 = new Trip {
+                Id = 2,
+                DestinationId = 2,
+                AccommodationId = 1,
+                StartDate = new DateTime(2020, 5, 23),
+                EndDate = new DateTime(2020, 6, 1),
+                Activities = new List<Activity> {act1, act3}
+            };
+
+            var trips = new List<Trip> {
+                trip1, trip2
+            };
+            var accRepo = new Mock<Repository<Accommodation>>();
+            var actRepo = new Mock<Repository<Activity>>();
+            var destRepo = new Mock<Repository<Destination>>();
+            var tripRepo = new Mock<Repository<Trip>>();
+
+            accRepo.Setup(repo => repo.Get(null)).Returns(accs.AsQueryable());
+            actRepo.Setup(repo => repo.Get(null)).Returns(acts.AsQueryable());
+            destRepo.Setup(repo => repo.Get(null)).Returns(dests.AsQueryable());
+            tripRepo.Setup(repo => repo.Get(null)).Returns(trips.AsQueryable());
+
+            unitOfWork.Setup(w => w.Accommodations).Returns(accRepo.Object);
+            unitOfWork.Setup(w => w.Activities).Returns(actRepo.Object);
+            unitOfWork.Setup(w => w.Destinations).Returns(destRepo.Object);
+            unitOfWork.Setup(w => w.Trips).Returns(tripRepo.Object);
+            return unitOfWork;
+        }
+
+        public static Mock<IHttpContextAccessor> SetUpHttpContextAccessor()
+        {
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            var context = new Mock<HttpContext>();
+            context.Setup(c => c.Session).Returns(new FakeSession());
+
+            var session = new FakeSession();
+            httpContextAccessor.Setup(a => a.HttpContext).Returns(context.Object);
+            return httpContextAccessor;
+        }
+
+        #endregion
+    }
+}
