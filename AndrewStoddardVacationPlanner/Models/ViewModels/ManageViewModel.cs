4c1a48459cd6e39fddb3cd51eb7138a10803a95b
AndrewStoddard <acs8182000@gmail.com>
2021-04-08 21:57:43 -0400

    Added a viewmodel for Manage page

---
+++ AndrewStoddardVacationPlanner/Models/ViewModels/ManageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using AndrewStoddardVacationPlanner.Models.DomainModels;
+
+namespace AndrewStoddardVacationPlanner.Models.ViewModels
+{
+    public class ManageViewModel
+    {
+        #region Properties
+
+        public string AccommodationName { get; set; }
+        public string AccommodationEmail { get; set; }
+        public string AccommodationPhone { get; set; }
+        public string ActivityName { get; set; }
+        public string DestinationName { get; set; }
+
+        public List<Destination> Destinations { get; set; }
+        public List<Activity> Activities { get; set; }
+        public List<Accommodation> Accommodations { get; set; }
+
+        public int SelectedActivityToDelete { get; set; }
+        public int SelectedAccommodationToDelete { get; set; }
+        public int SelectedDestinationToDelete { get; set; }
+
+        #endregion
+    }
+}
